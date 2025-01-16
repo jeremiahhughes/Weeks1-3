6ec80ad6883ffe9ed901863001b5f6ebc71f05ce
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2025-01-16 10:09:48 -0500

    CodingGym challenge done

---
+++ Assets/Scripts/Rotating.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
+}
